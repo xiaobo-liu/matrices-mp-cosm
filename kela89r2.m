e3fad20c2aae4e192cdaf7c184385604767253c4
Xiaobo Liu <xliu@mpi-magdeburg.mpg.de>
2025-05-21 18:05:45 +0200

    fix typo

---
+++ kela89r2.m
@@ -0,0 +1,7 @@
+function A = kela89r2
+%kela89r2   A 2-by-2 matrix from \cite[Ex.~5]{kela89}.
+
+%   C. S. Kenney and A. J. Laub. Condition Estimates for Matrix Functions. 
+%   SIAM J. Matrix Anal. Appl., 10(2):191–209, 1989.
+
+A = [0 1e6; 0 0];
--- kela98r3.m
+++ kela98r3.m
@@ -1,8 +1,8 @@
 function A = kela98r3
-%kela98r3   A 2-by-2 matrix from \cite[Ex.~5]{kela98}.
+%kela98r3   A 2-by-2 matrix from \cite[Ex.~3]{kela98}.
 
 %   C. S. Kenney and A. J. Laub. A Schur--Fréchet Algorithm for Computing 
 %   the Logarithm and Exponential of a Matrix. SIAM J. Matrix Anal. Appl.,
 %   19(3):640–663, 1998.
 
-A = [0 1e6; 0 0];
+A = [-1 1e7; 0 -1e7];
